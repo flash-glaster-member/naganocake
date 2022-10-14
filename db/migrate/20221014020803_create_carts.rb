45a7636b169163a64e60180be7578b7ac78fe168
masashi005 <111562823+masashi005@users.noreply.github.com>
2022-10-14 03:33:20 +0000

    create tables

--- db/migrate/20221014020803_create_carts.rb
+++ db/migrate/20221014020803_create_carts.rb
@@ -1,6 +1,9 @@
 class CreateCarts < ActiveRecord::Migration[6.1]
   def change
     create_table :carts do |t|
+      t.integer :product_id
+      t.integer :user_id
+      t.integer :volume
 
       t.timestamps
     end
