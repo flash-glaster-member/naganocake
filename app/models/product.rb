f9d332e1fc78e97dab2cf52e155d7c6553f10528
masashi005 <111562823+masashi005@users.noreply.github.com>
2022-10-15 10:32:21 +0000

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,10 @@ class Product < ApplicationRecord
   has_many :carts
   has_many :order_details
   belongs_to :genre
+  has_one_attached :product_image
+  
+  def get_product_image
+    (product_image.attached?) ? product_image : "sample.png"
+  end
+  
 end
