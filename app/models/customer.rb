0e8c3dda80a08b3da2daed23fa3ea7d08c086cf9
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-10-16 09:59:41 +0000

    create

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,20 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         validates :firstname, presence: true
+         validates :lastname, presence: true
+         validates :kana_firstname, presence: true
+         validates :kana_lastname, presence: true
+         validates :postcode, presence: true
+         validates :address, presence: true
+         validates :email, presence: true
+         validates :password, presence: true
+         
 
          has_many :carts
          has_many :orders
          has_many :addresses
+
+
 end
