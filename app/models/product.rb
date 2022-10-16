c501325dd88b22e3e09798446d99661671cdbf0f
masashi005 <111562823+masashi005@users.noreply.github.com>
2022-10-16 05:25:00 +0000

    update column

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ApplicationRecord
   has_many :order_details
   belongs_to :genre
   has_one_attached :product_image
-  
+
   def get_product_image
     (product_image.attached?) ? product_image : "sample.png"
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_15_082918) do
+ActiveRecord::Schema.define(version: 2022_10_16_051517) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "customer_id"
     t.string "send_name"
     t.text "send_address"
     t.integer "post_code"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_10_15_082918) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "product_id"
-    t.integer "user_id"
+    t.integer "customer_id"
     t.integer "volume"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 2022_10_15_082918) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "customer_id"
     t.integer "post_code"
     t.text "send_address"
     t.string "send_name"
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 2022_10_15_082918) do
     t.string "product_image"
     t.text "product_description"
     t.integer "product_price"
-    t.integer "sales_status"
+    t.boolean "sales_status"
   end
 
 end
