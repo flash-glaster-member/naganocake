27be624ccbd194b3702a9d9a15fb8bb21ba67b26
EC2 Default User <ec2-user@ip-172-31-0-52.ap-northeast-1.compute.internal>
2022-10-16 07:31:37 +0000

    モデル準備

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_15_082918) do
+ActiveRecord::Schema.define(version: 2022_10_14_182407) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.string "send_name"
-    t.text "send_address"
-    t.integer "post_code"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -41,56 +37,14 @@ ActiveRecord::Schema.define(version: 2022_10_15_082918) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "customers", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_customers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
-  end
-
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "genre_name"
-  end
-
-  create_table "order_details", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "product_id"
-    t.integer "oder_id"
-    t.integer "product_price"
-    t.integer "volume"
-    t.integer "production_status"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.integer "post_code"
-    t.text "send_address"
-    t.string "send_name"
-    t.integer "total_price"
-    t.integer "pay_way"
-    t.integer "oder_status"
-    t.integer "postage"
   end
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "genre_id"
-    t.string "product_name"
-    t.string "product_image"
-    t.text "product_description"
-    t.integer "product_price"
-    t.integer "sales_status"
   end
 
 end
