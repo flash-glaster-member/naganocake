3426489d5674039826fbfde2aad80f1353a76a77
masashi005 <111562823+masashi005@users.noreply.github.com>
2022-10-17 05:08:46 +0000

    update order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_16_171419) do
+ActiveRecord::Schema.define(version: 2022_10_17_034940) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
