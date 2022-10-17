4f0d4e024561aa8166f292ab7047e7faeb0d8f18
EC2 Default User <ec2-user@ip-172-31-0-52.ap-northeast-1.compute.internal>
2022-10-17 03:46:16 +0000

    add customer showpage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,8 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
@@ -13,6 +15,12 @@ class Public::CustomersController < ApplicationController
   end
 
   def out
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:lastname, :firstname, :kana_lastname, :kana_firstname, :postcode, :address)
   end
 
 end
