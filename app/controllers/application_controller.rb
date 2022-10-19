803a232b1cdb42b6138dc01710b8bedc07239c7b
EC2 Default User <ec2-user@ip-172-31-0-52.ap-northeast-1.compute.internal>
2022-10-19 02:40:01 +0000

    view fix misato

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+   def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :kana_first_name, :kana_last_name, :postcode, :address, :email, :password])
+   end
+
+   def after_sign_up_path_for(resource)
+      customers_path
+   end
+
+   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname], [:firstname], [:kana_last_name], [:kana_first_name])
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,20 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :cart_products
-         has_many :orders
-         has_many :addresses
+         has_many :cart_products, dependent: :destroy
+         has_many :orders, dependent: :destroy
+         has_many :addresses, dependent: :destroy
+                 
+         validates :firstname, presence: true
+         validates :lastname, presence: true
+         validates :kana_firstname, presence: true
+         validates :address, presence: true
+         validates :email, presence: true
+         validates :password, presence: true
+        
+  
+
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
